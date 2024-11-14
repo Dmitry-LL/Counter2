e531b1be7d6a738b67192c4993b71ae82c82eafe
Dmitry-LL <kli9k@yandex.ru>
2024-11-14 14:18:03 +0300

    Создан завершен

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter: Int = 0
+    @IBAction func CounterButton(_ sender: Any) {
+        print("Push")
+        counter += 1
+        CounterText.text = "Счетчик: \(counter)"
+    }
+    @IBOutlet weak var CounterText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
